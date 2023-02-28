c0ac4f651e84c90d36c3d905dcacca0bc036070f
Paul Wagner <p.wagner1120@icloud.com>
2023-02-28 08:51:57 -0600

    adding relationships exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -2,53 +2,58 @@ package com.codeup.codeupspringblog.controllers;
 
 
 import com.codeup.codeupspringblog.dao.PostRepository;
+import com.codeup.codeupspringblog.dao.UserRepository;
 import com.codeup.codeupspringblog.models.Post;
+import com.codeup.codeupspringblog.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class PostController {
 
+    private final UserRepository userDao;
     private final PostRepository postDao;
 
-    public PostController(PostRepository postDao){
+    public PostController(UserRepository userDao, PostRepository postDao){
+        this.userDao = userDao;
         this.postDao = postDao;
     }
-//    @GetMapping("/posts")
-//    public String posts(Model model){
-//        Post post = new Post(1, "New Post", "Look at this new post");
-//        Post post1 = new Post(2, "New Post 1", "Another new post");
-//        ArrayList <Post> allPosts = new ArrayList<>();
-//        allPosts.add(post);
-//        allPosts.add(post1);
-//        model.addAttribute("posts", allPosts);
-//        return "/posts/index";
-//    }
 
     @GetMapping("/posts")
-    public String posts(Model model){
+    public String postsHome(Model model){
+//        List<Post> posts = postDao.findAll();
+//        model.addAttribute("posts", posts);
         model.addAttribute("posts", postDao.findAll());
-        return "/posts/index";
+        return "posts/index";
+    }
+
+    @GetMapping("/posts/search")
+    public String showAllPosts(@RequestParam String query, Model model){
+        model.addAttribute("posts", postDao.searchByTitleLike(query));
+        return "posts/index";
     }
 
 
     @GetMapping("/posts/{id}")
-    public String singlePost(@PathVariable long id, Model model){
-        Post post = new Post(id, "New Post", "Look at this new post");
-        model.addAttribute("post", post);
-        return "/posts/show";
+    public String postsHome(@PathVariable long id, Model model){
+//        System.out.println(postDao.findPostById(id));
+        model.addAttribute("post", postDao.findPostById(id));
+        model.addAttribute("userIsCreator", true);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
-    public String create(){
-
+    public String postsForm(){
         return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    public String createPost(@RequestParam String title, @RequestParam String body) {
-        Post post = new Post(title, body);
+    public String createPost(@RequestParam String title, @RequestParam (name = "body") String body) {
+        User user = userDao.findUserById(1);
+        Post post = new Post(title, body, user);
         postDao.save(post);
         return "redirect:/posts";
     }
--- src/main/java/com/codeup/codeupspringblog/dao/PostRepository.java
+++ src/main/java/com/codeup/codeupspringblog/dao/PostRepository.java
@@ -2,6 +2,14 @@ package com.codeup.codeupspringblog.dao;
 
 import com.codeup.codeupspringblog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Post findPostByTitle(String title);
+    Post findPostById(long id);
+    @Query("from Post p where p.title like %:term%")
+    List<Post> searchByTitleLike(@Param("term") String term);
 }
